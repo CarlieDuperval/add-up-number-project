95a4df5b03ae4ec639c24398db29efd0fb6691d0
Carlie Duperval <carlierichell@gmail.com>
2022-04-24 10:36:18 -0400

    New added : Sum of number in array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// given a function add to numbers in an array
-function addUpNumber(arr){
+// Write a function thate given an array of numbers returns the sum of all numbers
+
+const numbers = [12, 4, 5, 7, 9]
+function addUptoNumbers(arr){
     let sum = 0
     for(let i = 0; i < arr.length ; i++ ){
         sum += arr[i] // sum = sum + arr[i]
@@ -7,7 +9,7 @@ function addUpNumber(arr){
     return sum
 }
 
-console.log(addUpNumber([12,4,5,7,9]))
+console.log('The total number is : ', addUptoNumbers(numbers))
 
 
 
